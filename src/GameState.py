02a61f7d418d7b019e40a60cde6b3f4da1b260d5
Matheus Pissaia <pissaia.matheus03@gmail.com>
2026-05-03 22:23:56 -0300

    chore: enhance comments for clarity in GameState class

--- src/GameState.py
+++ src/GameState.py
@@ -13,10 +13,15 @@ goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 class GameState:
     def __init__(self, board, empty_index, depth, parent=None, move=None):
+        # Board grid represented as a 1D array
         self.board: list[int] = board
+        # Index of the empty tile
         self.empty_index: int = empty_index
+        # Search depth of the current state
         self.depth: int = depth
+        # Game state parent node
         self.parent: "GameState | None" = parent
+        # Move made to reach the current state
         self.move: "Move | None" = move
 
     @property
